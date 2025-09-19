05a2c526d4f86ce83a8abf192ff470aeccff499d
Joe <cybo42@gmail.com>
2025-09-18 23:31:43 -0400

    Added reverse function

--- cmd/root.go
+++ cmd/root.go
@@ -10,6 +10,9 @@ import (
 // Version is set during build using ldflags
 var Version = "dev"
 
+// Flag to determine if output should be reversed
+var reverseOutput bool
+
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:     "weather-cli",
@@ -18,7 +21,17 @@ var rootCmd = &cobra.Command{
 	Long:    `A simple CLI application that prints "hello world".`,
 	// Run is the function that will be called when the command is executed
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("hello world")
+		if reverseOutput {
+			// Reverse "hello world" and print it
+			message := "hello world"
+			runes := []rune(message)
+			for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+				runes[i], runes[j] = runes[j], runes[i]
+			}
+			fmt.Println(string(runes))
+		} else {
+			fmt.Println("hello world")
+		}
 	},
 }
 
@@ -29,4 +42,9 @@ func Execute() {
 	if err != nil {
 		os.Exit(1)
 	}
+}
+
+func init() {
+	// Add the reverse flag to the root command
+	rootCmd.Flags().BoolVarP(&reverseOutput, "reverse", "r", false, "Print hello world backwards")
 }
